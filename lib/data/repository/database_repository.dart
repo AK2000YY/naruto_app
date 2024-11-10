7790bfb4a9a0af854c60e9ea9b5a0bba0caca592
Abdulkarim <abdyabkor@gmail.com>
2024-11-10 21:28:20 +0300

    modify database repository.

--- lib/data/repository/database_repository.dart
+++ lib/data/repository/database_repository.dart
@@ -5,7 +5,7 @@ class DatabaseRepository {
 
   Database? _db;
 
-  Future<Database?> open() async {
+  Future<Database?> _open() async {
     if(_db != null) return _db;
     _db = await openDatabase(
       join(await getDatabasesPath(), "naruto_app.db"),
@@ -16,24 +16,43 @@ class DatabaseRepository {
   }
 
   _onCreate(Database? db, int version) async {
-    await db?.execute("CREATE TABLE characters(id INTEGER PRIMARY KEY, name TEXT");
-    await db?.execute("CREATE TABLE clans(id INTEGER PRIMARY KEY, name TEXT");
-    await db?.execute("CREATE TABLE villages(id INTEGER PRIMARY KEY, name TEXT");
-    await db?.execute("CREATE TABLE kekkei_genkai(id INTEGER PRIMARY KEY, name TEXT");
-    await db?.execute("CREATE TABLE tailed_beasts(id INTEGER PRIMARY KEY, name TEXT");
-    await db?.execute("CREATE TABLE teams(id INTEGER PRIMARY KEY, name TEXT");
-    await db?.execute("CREATE TABLE akatsuki(id INTEGER PRIMARY KEY, name TEXT");
+    await db?.execute("CREATE TABLE characters(id INTEGER PRIMARY KEY, name TEXT)");
+    await db?.execute("CREATE TABLE clans(id INTEGER PRIMARY KEY, name TEXT)");
+    await db?.execute("CREATE TABLE villages(id INTEGER PRIMARY KEY, name TEXT)");
+    await db?.execute("CREATE TABLE kekkei_genkai(id INTEGER PRIMARY KEY, name TEXT)");
+    await db?.execute("CREATE TABLE tailed_beasts(id INTEGER PRIMARY KEY, name TEXT)");
+    await db?.execute("CREATE TABLE teams(id INTEGER PRIMARY KEY, name TEXT)");
+    await db?.execute("CREATE TABLE akatsuki(id INTEGER PRIMARY KEY, name TEXT)");
   }
 
   insertData<T>(List<T> items, String tableName,Map<String, Object?> Function(T) toMap) async {
-    for (var item in items) {
-      await _db?.insert(tableName, toMap(item));
+    await _open();
+    try {
+      await _db?.transaction((it) async {
+        var batch = it.batch();
+        for(var item in items) {
+          batch.insert(tableName, toMap(item));
+        }
+        await batch.commit(noResult: true);
+      });
+    } catch(err) {
+      throw Exception("Failed to insert elements");
     }
   }
 
   Future<List<T>?> getData<T>(String tableName, T Function(Map<String, Object?> item) fromMap) async {
-    List<Map<String, Object?>>? result = await _db?.query(tableName);
-    return result?.map((it) => fromMap(it)).toList();
+    await _open();
+    try {
+      List<Map<String, Object?>>? result = await _db?.query(tableName);
+      return result?.map((it) => fromMap(it)).toList();
+    } catch(err) {
+      throw Exception("Failed to get elements");
+    }
+  }
+
+  close() async {
+    await _db?.close();
+    _db = null;
   }
 
 }
